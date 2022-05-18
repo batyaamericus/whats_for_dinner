3ad906e69ebe647624ff8b53a9175bdc751368f5
Batya Americus <batya.americus@gmail.com>
2022-05-18 23:46:23 +0300

    populating bets table with percentages

--- model/db_populate_bets.py
+++ model/db_populate_bets.py
@@ -5,27 +5,16 @@ import config
 from tqdm.auto import tqdm
 
 
-df = pd.read_csv('nba_elo.csv')
-as_datetime = pd.to_datetime(df['date'])
-date_bound = pd.Timestamp('2010-01-01 00:00:00')
-df.loc[:, 'date_as_dt'] = as_datetime
-df.drop('date', axis=1)
-df = df[df['date_as_dt'] > date_bound]
-df = df.reset_index().drop('index', axis=1)
-
+df = pd.read_csv('initial_preds.csv')
 engine = create_engine(config.engine_url, future=True)
-cols = ['team1', 'team2', 'date']
 
 with engine.connect() as conn:
-    for i in tqdm(range(14000, 16046)):
-        game_id = ''.join([df.loc[i, col] for col in cols])
+    for i in tqdm(range(df.shape[0])):
+        game_id = df.loc[i, 'uid']
         teamName = df.loc[i, 'team1']
-        team2Name = df.loc[i, 'team2']
+        our_prediction = df.loc[i, 'prob1']
 
-        stmt = f'''INSERT INTO bets (game_id, teamName) VALUES ('{game_id}', '{teamName}')'''
-        print(stmt)
-        conn.execute(text(stmt))
-        stmt = f'''INSERT INTO bets (game_id, teamName) VALUES ('{game_id}', '{team2Name}')'''
+        stmt = f'''INSERT INTO bets (game_id, teamName, our_prediction) VALUES ('{game_id}', '{teamName}', '{our_prediction}')'''
         print(stmt)
         conn.execute(text(stmt))
         conn.commit()
