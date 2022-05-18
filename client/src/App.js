d934fe50191b51f0f4ed2407cf514de2ef327a2b
123quatrecinq <96394817+AmielCo@users.noreply.github.com>
2022-05-18 15:02:16 +0300

    userprofile

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/auth" element={<Auth />} />
+        
       </Routes>
     </AuthProvider>
   );
