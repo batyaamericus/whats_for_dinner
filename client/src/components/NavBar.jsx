d9cbeddef9536d9433d8aa2e3058ebcd2bf533d1
123quatrecinq <96394817+AmielCo@users.noreply.github.com>
2022-05-18 11:48:34 +0300

    new empty pages

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,33 +1,35 @@
 import React from 'react'
-import { Nav, NavDropdown } from "react-bootstrap";
+import { Container, Nav, NavDropdown } from "react-bootstrap";
 
 const NavBar = () => {
     const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
-  return (
-   <Nav variant="pills" activeKey="1" onSelect={handleSelect}>
-      <Nav.Item>
-        <Nav.Link eventKey="1" href="#/home">
-       Top 10 Bets
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="2" title="Item">
-          NavLink 2 content
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="3" disabled>
-          NavLink 3 content
-        </Nav.Link>
-      </Nav.Item>
-      <NavDropdown title="Dropdown" id="nav-dropdown">
-        <NavDropdown.Item eventKey="4.1">Register</NavDropdown.Item>
-        <NavDropdown.Item eventKey="4.2">Login</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item eventKey="4.4">Logout</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
-  );
+    return (
+      <Container className="center">
+        <Nav variant="pills" activeKey="1" onSelect={handleSelect}>
+          <Nav.Item>
+            <Nav.Link eventKey="1" href="#/home">
+              Top 10 Bets
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="2" title="Item">
+              NavLink 2 content
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="3" disabled>
+              NavLink 3 content
+            </Nav.Link>
+          </Nav.Item>
+          <NavDropdown title="Dropdown" id="nav-dropdown">
+            <NavDropdown.Item eventKey="4.1">Register</NavDropdown.Item>
+            <NavDropdown.Item eventKey="4.2">Login</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item eventKey="4.4">Logout</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Container>
+    );
 }
   
 
