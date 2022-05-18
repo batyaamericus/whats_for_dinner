49b4018a6fa3b9b10dc43f3e22582b9c9baa7c28
alanlipschutz <alanelipschutz@gmail.com>
2022-05-18 21:05:27 +0300

    pulling BE

--- client/src/pages/TopTen.jsx
+++ client/src/pages/TopTen.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { Col } from "react-bootstrap";
+import { Row } from "react-bootstrap";
 import { Container } from "react-bootstrap";
+import ItemResult from "../components/ItemResult.js";
 import { getTopTenResults } from "../services/services.js";
 
 function TopTen() {
@@ -11,6 +14,7 @@ function TopTen() {
     try {
       const res = await getTopTenResults();
       console.log(res);
+      setResults(res);
     } catch (error) {}
   }
 
@@ -18,7 +22,22 @@ function TopTen() {
     getResults();
   }, []);
 
-  return <Container>hola</Container>;
+  return (
+    <Container>
+      <Row className="mt-3 justify-content-center">
+        {results.map((result) => (
+          <Col
+            key={result.game_id}
+            md={"auto"}
+            lg={"auto"}
+            style={{ width: "auto" }}
+          >
+            <ItemResult result={result} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
 }
 
 export default TopTen;
