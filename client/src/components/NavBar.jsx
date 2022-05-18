067af125dd230118082d9cf57815963262ee66cb
123quatrecinq <96394817+AmielCo@users.noreply.github.com>
2022-05-18 13:56:16 +0300

    nav

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,62 +1,59 @@
 import React from "react";
-import {Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
-import {Link} from "react-router-dom";
+import { Navbar, Container, Nav, NavDropdown, Stack } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
 
 const NavBar = () => {
   return (
-    <Container className="navCont center">
-      <Navbar className="nav px-2" expand="lg">
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className=" nav2 me-auto">
-          
-         
-                <Link
-                  style={{ textDecoration: "none" }}
-                  className="styleNavLink navHome px-5"
-                  to="/"
-                >
-                  Home
-                </Link>
-                <Link
-                  style={{ textDecoration: "none" }}
-                  className="styleNavLink navProfile px-5"
-                  to="/"
-                >
-                  Top 10
-                </Link>
-           
-              <Link
-                style={{ textDecoration: "none" }}
-                className="styleNavLink navProfile px-5"
-                to="/"
-              >
-                Login
-              </Link>
-            
-           
-             {/*  <Link
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Wise Bet</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Link
+              style={{ textDecoration: "none" }}
+              className="styleNavLink navHome px-5 "
+              to="/"
+            >
+              Home
+            </Link>
+            <Link
+              style={{ textDecoration: "none" }}
+              className="styleNavLink navProfile px-5"
+              to="/"
+            >
+              Top 10
+            </Link>
+          </Nav>
+          <Nav>
+           {/*  <Link
+              style={{ textDecoration: "none" }}
+              className="styleNavLink navProfile px-5 "
+              to="/auth"
+            >
+              Login
+            </Link> */}
+
+            {/*  <Link
                 style={{ textDecoration: "none" }}
                 className="styleNavLink navProfile px-5"
                 to="/signin"
               >
                 Sign in
               </Link> */}
-            
-           
-              <Link
-                to="/#"
-                style={{ textDecoration: "none" }}
-                className="styleNavLink navHome px-5"
-              
-              >
-                Logout
-              </Link>
-            
+
+            <Link
+              to="/#"
+              style={{ textDecoration: "none" }}
+              className="styleNavLink navHome px-5"
+            >
+              Logout
+            </Link>
           </Nav>
         </Navbar.Collapse>
-      </Navbar>
-    </Container>
+      </Container>
+    </Navbar>
   );
 };
 
