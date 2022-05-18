c019dd68cc38549ce23b57064b74b9a19ae35aa7
alanlipschutz <alanelipschutz@gmail.com>
2022-05-18 16:49:31 +0300

    change style in itemResult

--- client/src/components/ItemResult.js
+++ client/src/components/ItemResult.js
@@ -3,8 +3,8 @@ import { Card } from "react-bootstrap";
 
 function ItemResult({ result }) {
   return (
-    <Card bg="success" style={{ width: "18rem" }} className="mb-2">
-      <Card.Header>Team A vs Team B</Card.Header>
+    <Card bg="dark" style={{ width: "18rem" }} className="mb-2">
+      <Card.Header style={{ color: "#FFB22A" }}>Team A vs Team B</Card.Header>
       <Card.Body>
         <Card.Title>Probability </Card.Title>
       </Card.Body>
