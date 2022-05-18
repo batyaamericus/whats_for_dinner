fdf4b5c5a965f838d4ec8a19663d122e77b7f7e7
alanlipschutz <alanelipschutz@gmail.com>
2022-05-18 15:31:42 +0300

    search page added

--- client/src/pages/Search/Search.jsx
+++ client/src/pages/Search/Search.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
+import { FormLabel } from "react-bootstrap";
 import { Button, FormControl, Form, Container } from "react-bootstrap";
 
 function Search(props) {
   const [results, setResults] = useState([]);
   return (
-    <Container>
-      <Form className="d-flex">
+    <Container className="mt-4">
+      <h2 className="display-5">Search your team</h2>
+      <Form className="d-flex mt-3">
         <FormControl
           type="search"
           placeholder="Search"
