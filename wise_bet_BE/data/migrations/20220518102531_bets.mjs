40f5509fe117163079640f7619727aab3bff6c17
Valerii Lamm <lammvalerii@gmail.com>
2022-05-18 15:29:42 +0300

    migrations

--- wise_bet_BE/data/migrations/20220518102531_bets.mjs
+++ wise_bet_BE/data/migrations/20220518102531_bets.mjs
@@ -2,15 +2,11 @@
 export function up(knex) {
     return knex.schema.createTable('bets', function (table) {
       table.uuid('entry_id').primary().defaultTo(knex.raw("(UUID())"));
-      table.date('date').notNull();
+      table.string('game_id').references('game_id').inTable('games');
       table.string('teamName').notNull();
-      table.integer('team_1_wins_dk_odds').notNull();
-      table.integer('team_1_wins_dk_persentage').notNull();
-      table.integer('team_1_wins_our_prediction').notNull();
-      table.string('team_2').notNull();
-      table.integer('team_2_wins_dk_odds').notNull();
-      table.integer('team_2_wins_dk_persentage').notNull();
-      table.integer('team_2_wins_our_prediction').notNull();
+      table.integer('dk_odds').notNull();
+      table.integer('dk_persentage').notNull();
+      table.integer('our_prediction').notNull();
     });
   }
   export function down(knex) {
