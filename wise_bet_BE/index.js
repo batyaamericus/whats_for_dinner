ee1728a3a2b34a8d1886fcac79e1e98006def349
MeirRozenbaum <97239582+MeirRozenbaum@users.noreply.github.com>
2022-05-18 12:40:00 +0300

    start2

--- wise_bet_BE/index.js
+++ wise_bet_BE/index.js
@@ -1,6 +1,12 @@
 import mysql from 'mysql'
 import express from 'express'
 const PORT = 8080;
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
 
 app.use(cors({ origin: ["*"], credentials: true }))
 app.use(express.json())
+
+app.use("/user", userRoute)
